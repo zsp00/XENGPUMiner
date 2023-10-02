b3d6ee03c10567941683769e88c175c5f9fef5ca
OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO <zhusipu123@163.com>
2023-10-02 12:20:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ pip install -U -r requirements.txt
 ./build.sh -cuda_arch sm_87
 
 screen -S gpu0
-./xengpuminer -d 0
+./xengpuminer -d 0 &&
 screen -S gpu1
-./xengpuminer -d 1
+./xengpuminer -d 1 &&
 screen -S gpu2
-./xengpuminer -d 2
+./xengpuminer -d 2 &&
 screen -S gpu3
-./xengpuminer -d 3
+./xengpuminer -d 3 &&
 screen -S miner
-python3 miner.py --gpu=true
+python3 miner.py --gpu=true &&
