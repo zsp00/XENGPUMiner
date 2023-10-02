9f1cb532bb884a2224c9681b6e25f7f360f7c3bd
OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO <zhusipu123@163.com>
2023-10-02 12:19:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,14 @@ cd XENGPUMiner
 chmod +x build.sh 
 pip install -U -r requirements.txt  
 ./build.sh -cuda_arch sm_87
+
+screen -S gpu0
+./xengpuminer -d 0
+screen -S gpu1
+./xengpuminer -d 1
+screen -S gpu2
+./xengpuminer -d 2
+screen -S gpu3
+./xengpuminer -d 3
+screen -S miner
+python3 miner.py --gpu=true
